b35999259dd3baddbb62d9a6f2dc7edbfbe45342
Naman Garg <namangarg155@gmail.com>
2020-04-13 19:59:41 +0530

    New Commit

--- tick.py
+++ tick.py
@@ -1,8 +1,12 @@
 def board(board):
     pass
 
-def player1_input():
-    int(input("Enter position for player 1"))
+def player1_input(player1):
+   player1 = int(input("Enter position for player 1"))
+
+def player2_input(player2):
+    player2=int(input("Enter position for player 2"))
+
 
 board=['1','2,'3','4','5','6' ,'7' ,'8' ,'9' ]
 
