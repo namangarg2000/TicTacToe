2fb49f977985d3b8e2f70477ef9d34f77dadc820
Naman Garg <namangarg155@gmail.com>
2020-04-13 20:30:29 +0530

    Updated

--- tick.py
+++ tick.py
@@ -1,12 +1,38 @@
-def board(board):
+def show_board():
+    print(board)
+
+def check():
     pass
 
-def player1_input(player1):
+def player1_input():
    player1 = int(input("Enter position for player 1"))
+   return player1
 
-def player2_input(player2):
+def player2_input():
     player2=int(input("Enter position for player 2"))
+    return player2
 
 
-board=['1','2,'3','4','5','6' ,'7' ,'8' ,'9' ]
+board=['1','2','3','4','5','6' ,'7' ,'8' ,'9' ]
 
+if __name__=='__main__':
+
+    choice=input("Start the game?(y/n)")
+    if(choice=='Y' or choice=='y'):
+        
+        print("Game starting")
+
+        while(choice=='Y' or choice=='y'):
+            board=['1','2','3','4','5','6' ,'7' ,'8' ,'9' ]
+            player1=player1_input()
+
+            board[player1-1]='X'
+            check()
+            show_board()
+
+            choice=input("Start a new game?(y/n)")
+
+
+    else:
+        #print("Game end")
+        exit(0)
