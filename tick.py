9811f270d0814b8af98c63d515b8dfe11041b2c5
Naman Garg <namangarg155@gmail.com>
2020-04-13 19:42:41 +0530

    Update tick.py

--- tick.py
+++ tick.py
@@ -1,3 +1,8 @@
-# Trying git
+def board(board):
+    pass
 
-print('Hello World')
+def player1_input():
+    int(input("Enter position for player 1"))
+
+board=['1','2,'3','4','5','6' ,'7' ,'8' ,'9' ]
+
